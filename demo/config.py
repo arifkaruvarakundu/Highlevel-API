14b5e7688aabf60e431d7884f844f90d2bbc5724
muhammed harif <harifn786@gmail.com>
2024-02-12 19:55:41 +0530

    Initial commit

---
+++ demo/config.py
@@ -0,0 +1,6 @@
+# config.py
+HIGHLEVEL_API_BASE_URL = "https://api.highlevel.com"
+HIGHLEVEL_CLIENT_ID = "65c0a9a4277b2961322c545a-ls8q934d"
+HIGHLEVEL_CLIENT_SECRET = "94af4663-c0c7-4340-9ce5-39b38e88c146"
+REDIRECT_URL = "http://127.0.0.1/"
+CUSTOM_FIELD_NAME = "DFS Booking Zoom Link"
